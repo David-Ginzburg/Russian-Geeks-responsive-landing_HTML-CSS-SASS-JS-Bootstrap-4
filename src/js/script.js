6b061c25b3b401bd58f32fdd61432ed8ca8fdb35
Vadim Mostovenko <vadim.mostovenko@gmail.com>
2021-02-27 22:54:31 +0300

    modals

--- src/js/script.js
+++ src/js/script.js
@@ -50,3 +50,22 @@ new WOW().init();
     svgColor: '#fff',
     closeColor: '#fff'}
 )
+
+/* youtube */
+
+function autoPlayYouTubeModal(){
+    let trigger = $("body").find('[data-toggle="modal"]')
+    trigger.click(function() {
+        let theModal = $(this).data( "target" ),
+        videoSRC = $(this).attr( "data-theVideo" ), 
+        videoSRCauto = videoSRC+"?autoplay=1" ;
+        $(theModal+' iframe').attr('src', videoSRCauto)
+        $(theModal+' button.close').click(function () {
+            $(theModal+' iframe').attr('src', videoSRC)
+        });
+    });
+}
+
+$(document).ready(function(){
+    autoPlayYouTubeModal()
+})
